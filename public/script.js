e72bdbb71cb23eedf5f306e42a5c72728fb0a266
JonathanMurray <jonte_murray@hotmail.com>
2015-04-06 23:39:16 +0200

    Prettier gui

--- public/script.js
+++ public/script.js
@@ -1,20 +1,20 @@
-function render_result(){
-	console.log("render_res");
+function query_and_render(){
 	var hashtag = $("#hashtag-input").val();
 	jQuery.ajax({
 		url: "https://twitter-rest.herokuapp.com/tweets/" + hashtag,
 		context: document.body,
 	}).done(function(data) {
 		var tweets = data["tweets"];
-		$("#result-section").html("");
-		$.each(tweets, function(index, tweet){
-			var user = tweet["user"];
-			var message = tweet["message"];
-			var html = (index+1) + ": " + tweet.user + " says '" + tweet.message + "'";
-			$("#result-section").append("<div>" + html + "</div>");
-		});
-		console.log(data);
+		render(tweets);
 	});
 }
 
-console.log("done reading script.js");
+function render(tweets){
+	$("#result-section").html("");
+	$.each(tweets, function(index, tweet){
+		var user = tweet["user"];
+		var message = tweet["message"];
+		var entry = "<span class='author'>" + tweet.user + "</span> <span class='message'>" + tweet.message + "</span>";
+		$("#result-section").append("<div class='entry'>" + entry + "</div>");
+	});
+}
